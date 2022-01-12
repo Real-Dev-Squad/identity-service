d98be79ff9e54c5b693022047b1e500154ee03f7
mehulkchaudhari <chaudharimehul2001@gmail.com>
2022-01-12 19:17:11 +0530

    connecting firestore

--- health/go.mod
+++ health/go.mod
@@ -1,4 +1,21 @@
-require github.com/aws/aws-lambda-go v1.23.0
+require (
+	cloud.google.com/go/firestore v1.6.1
+	cloud.google.com/go/storage v1.18.2 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/envoyproxy/go-control-plane v0.10.0 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.6.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	google.golang.org/api v0.63.0
+	google.golang.org/grpc v1.42.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
 
 module health
 
--- health/main.go
+++ health/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	// "errors"
+	"context"
 	"fmt"
-	"io/ioutil"
+	// "io/ioutil"
+	"log"
 
-	"net/http"
+	// "net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+	"google.golang.org/api/iterator"
+
 )
 
 var (
@@ -16,20 +23,63 @@ var (
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(memberurl)
+	// resp, err := http.Get(memberurl)
 
+	// if err != nil {
+	// 	return events.APIGatewayProxyResponse{}, err
+	// }
+
+	// r, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return events.APIGatewayProxyResponse{}, err
+	// }
+	// fmt.Printf("%v", string(r))
+
+	//TODO:save the response/err in firestore
+
+	// Use a service account
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("./firebase.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	client, err := app.Firestore(ctx)
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		log.Fatalln(err)
+	}
+	log.Println(app)
+// create data 
+
+defer client.Close()
+// [START firestore_setup_dataset_pt2]
+_, _, err = client.Collection("logs").Add(ctx, map[string]interface{}{
+	"first":  "Alan",
+	"middle": "Mathison",
+	"last":   "Turing",
+	"born":   1912,
+})
+if err != nil {
+	log.Fatalf("Failed adding aturing: %v", err)
+}
+
+
+
+	// read tdata 
+	iter := client.Collection("logs").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
 	}
 
-	r, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-	fmt.Printf("%v", string(r))
-	//save the response/err in firestore
 	return events.APIGatewayProxyResponse{
-		Body:       "Awesome, Your Server health is good!!!",
+		Body:       "Awesome, Your server health is good!!!!",
 		StatusCode: 200,
 	}, nil
 }
