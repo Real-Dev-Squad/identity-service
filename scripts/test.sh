4c885110046ebda252bc0ed6c8ce28d30b35a233
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2022-11-06 02:41:57 +0530

    exit script on error

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,11 +4,12 @@ go test -v
 npx kill-port 8090
 cd ../verify
 go mod tidy
-npx firebase --project="test" emulators:exec "go test"
+npm install -g firebase-tools
+firebase --project="test" emulators:exec "go test"
 npx kill-port 8090
 cd ../profile
 go mod tidy
-if (npx firebase --project="test" emulators:exec "go test"); then
+if (firebase --project="test" emulators:exec "go test"); then
     echo "Exited Success"
 else
     exit 1
