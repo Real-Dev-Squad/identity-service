f518a309bf5b9f6c14af509a0d7d4eadaeb3f6af
ruthvik <ruthv1k@proton.me>
2023-03-18 10:47:09 +0530

    Add initial mocking setup
    - Added MockClient in `utils/mocks/client.go`
    - Updated `verify` test file to use the mock http client

--- verify/main_test.go
+++ verify/main_test.go
@@ -2,18 +2,38 @@ package main
 
 import (
 	"context"
+	"io"
 	"os"
 	"testing"
+	"verify/utils/mocks"
 
 	"cloud.google.com/go/firestore"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
 
 	"fmt"
+	"net/http"
+
 	"google.golang.org/api/iterator"
 )
 
+func init() {
+	Client = &mocks.MockClient{}
+}
+
 func TestHandler(t *testing.T) {
+	// testing the mock func
+	mocks.PostFunc = func(url string, contentType string, body io.Reader) (*http.Response, error) {
+		if url == "https://96phoonyw3.execute-api.us-east-2.amazonaws.com/Prod" {
+			return &http.Response{
+				StatusCode: 200,
+			}, nil
+		}
+
+		return &http.Response{
+			StatusCode: 500,
+		}, fmt.Errorf("mock error")
+	}
 
 	os.Setenv("environment", "test")
 	defer os.Unsetenv("environment")
@@ -34,16 +54,16 @@ func TestHandler(t *testing.T) {
 	fmt.Println("All users:")
 	iter := client.Collection("users").Documents(ctx)
 	for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-					break
-			}
-			if err != nil {
-					break
-			}
-			RefId = doc.Ref.ID
-			fmt.Println(doc.Ref.ID)
-			fmt.Println(doc.Data())
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			break
+		}
+		RefId = doc.Ref.ID
+		fmt.Println(doc.Ref.ID)
+		fmt.Println(doc.Data())
 	}
 
 	tests := []struct {
