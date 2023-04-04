9905aa39794ad85035e4c67c1422836e7ae01610
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-04-04 09:47:43 +0530

    fix validation function

--- profile/main.go
+++ profile/main.go
@@ -227,17 +227,17 @@ func initializeFirestoreClient(ctx context.Context) (*firestore.Client, error) {
 
 func (res Res) Validate() error {
 	return validation.ValidateStruct(&res,
-			validation.Field(&res.FirstName, validation.Required),
-			validation.Field(&res.LastName, validation.Required),
-			validation.Field(&res.Phone, validation.Required, is.Digit),
-			validation.Field(&res.Email, validation.Required, is.Email),
-			validation.Field(&res.YOE, validation.Min(0)),
-			validation.Field(&res.Company, validation.Required),
-			validation.Field(&res.Designation, validation.Required),
-			validation.Field(&res.GithubId, validation.Required),
-			validation.Field(&res.LinkedIn, validation.Required),
-			validation.Field(&res.Website, is.URL)),
-		validation.Field(&res.Dob, validation.Required)
+		validation.Field(&res.FirstName, validation.Required),
+		validation.Field(&res.LastName, validation.Required),
+		validation.Field(&res.Phone, validation.Required, is.Digit),
+		validation.Field(&res.Email, validation.Required, is.Email),
+		validation.Field(&res.YOE, validation.Min(0)),
+		validation.Field(&res.Company, validation.Required),
+		validation.Field(&res.Designation, validation.Required),
+		validation.Field(&res.GithubId, validation.Required),
+		validation.Field(&res.LinkedIn, validation.Required),
+		validation.Field(&res.Website, is.URL),
+		validation.Field(&res.Dob, validation.Required))
 }
 
 /*
