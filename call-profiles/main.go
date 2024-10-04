b09a5240f76679fd1ad18e6f1b812aa448512347
JC CODER <josephchimezie2003@gmail.com>
2024-10-04 01:23:51 +0100

    update call-profiles to invoke call-profile using lambdaInvokd

--- call-profiles/main.go
+++ call-profiles/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"identity-service/layer/utils"
 	"log"
-	"net/http"
-	"os"
 	"sync"
 	"time"
 
@@ -20,24 +17,26 @@ import (
 var wg sync.WaitGroup
 
 func callProfile(userId string, sessionId string) {
+	log.Printf("Calling profile for user with ID: %s\n", userId)
 
 	defer wg.Done()
 
-	httpClient := &http.Client{}
-	jsonBody := []byte(fmt.Sprintf(`{"userId": "%s", "sessionId": "%s"}`, userId, sessionId))
-	bodyReader := bytes.NewReader(jsonBody)
+	payload := utils.ProfileLambdaCallPayload{
+		UserId:    userId,
+		SessionID: sessionId,
+	}
 
-	requestURL := fmt.Sprintf("%s/profile", os.Getenv("baseURL"))
-	req, _ := http.NewRequest(http.MethodPost, requestURL, bodyReader)
-	_, err1 := httpClient.Do(req)
-	if err1 != nil {
-		fmt.Println("error getting profile data", err1)
+	err := utils.InvokeProfileLambda(payload)
+	if err != nil {
+		log.Println("error calling profile lambda", err)
 	}
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	ctx := context.Background()
 	client, err := utils.InitializeFirestoreClient(ctx)
+
+	fmt.Println("Calling profiles - entry point")
 
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
@@ -53,7 +52,12 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	totalProfilesCalled := 0
 
+	// TODO: remove this
+	allUsers := client.Collection("users").Documents(ctx)
+	log.Println("All users", allUsers)
+
 	iter := client.Collection("users").Where("profileStatus", "==", "VERIFIED").Documents(ctx)
+	log.Println("Iterating over users", iter)
 	for {
 		doc, err := iter.Next()
 		if err == iterator.Done {
---
+++ layer/utils/lambdacalls.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+)
+
+type ProfileLambdaCallPayload struct {
+	UserId    string `json:"userId"`
+	SessionID string `json:"sessionId"`
+}
+
+func InvokeProfileLambda(payload ProfileLambdaCallPayload) error {
+	session := session.Must(session.NewSession())
+	client := lambda.New(session)
+
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("error marshalling payload: %w", err)
+	}
+
+	functionName := "CallProfileFunction"
+	fmt.Println("functionName", functionName)
+	if functionName == "" {
+		return fmt.Errorf("PROFILE_LAMBDA_FUNCTION_ARN is not set")
+	}
+
+	input := &lambda.InvokeInput{
+		FunctionName: aws.String(functionName),
+		Payload:      payloadBytes,
+	}
+
+	_, err = client.Invoke(input)
+	if err != nil {
+		fmt.Println("error invoking lambda", err)
+		return fmt.Errorf("error invoking lambda: %w", err)
+	}
+
+	return nil
+}
