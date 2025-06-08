a4c13257e0f451e6d03914bcffce08ac9b02acf2
vinit bliro <v.khandal@bliro.io>
2025-06-09 00:30:05 +0530

    fix: update dev.sh to support cross-platform

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,9 @@
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+    SAM_CMD="sam.cmd"
+else
+    SAM_CMD="sam"
+fi
+
 go mod tidy
-sam.cmd build
-sam.cmd local start-api --env-vars env.json
+$SAM_CMD build
+$SAM_CMD local start-api --env-vars env.json
